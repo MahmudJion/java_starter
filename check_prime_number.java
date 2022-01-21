b477d985ce9830249478f4477cfc9751878b8f27
MahmudJion <mahmudh.jion02@gmail.com>
2022-01-21 20:02:24 +0000

    update with input method

--- check_prime_number.java
+++ check_prime_number.java
@@ -1,20 +1,25 @@
-public class Main {
+import java.util.Scanner;
+
+class Main {
 
     public static void main(String[] args) {
 
-      int num = 29;
-      boolean flag = false;
-      for (int i = 2; i <= num / 2; ++i) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num = reader.nextInt();
 
-        if (num % i == 0) {
-          flag = true;
-          break;
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
         }
-      }
 
-      if (!flag)
-        System.out.println(num + " is a prime number.");
-      else
-        System.out.println(num + " is not a prime number.");
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
     }
-  }
+}
