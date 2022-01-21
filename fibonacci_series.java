65b7748736f285bcb0b26f1deba01615fb4367ed
MahmudJion <mahmudh.jion02@gmail.com>
2022-01-21 20:11:17 +0000

    update with input method

--- fibonacci_series.java
+++ fibonacci_series.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
 
-        int n = 10, firstTerm = 0, secondTerm = 1;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter number of term: ");
+        System.out.println("Enter first term: ");
+        System.out.println("Enter second term: ");
+
+        int n = reader.nextInt();
+        int firstTerm = reader.nextInt();
+        int secondTerm = reader.nextInt();
         System.out.println("Fibonacci Series till " + n + " terms:");
 
         for (int i = 1; i <= n; ++i) {
