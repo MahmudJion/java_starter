32e349692199b8aa356702279acc8b5484d92ec1
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-01 22:22:44 +0000

    fixed the scanner reader issue

--- add_two_numbers.java
+++ add_two_numbers.java
@@ -3,12 +3,13 @@ class Main {
 
   public static void main(String[] args) {
 
-    Scanner reader = new Scanner(System.in);
-    System.out.println("Enter two numbers");
-    int first = reader.nextInt();
-    int second = reader.nextInt();
+    try (Scanner reader = new Scanner(System.in)) {
+        System.out.println("Enter two numbers");
+        int first = reader.nextInt();
+        int second = reader.nextInt();
 
-    int sum = first + second;
-    System.out.println("The sum is: " + sum);
+        int sum = first + second;
+        System.out.println("The sum is: " + sum);
+    }
   }
 }
--- fibonacci_series.java
+++ fibonacci_series.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Enter number of term: ");
-        System.out.println("Enter first term: ");
-        System.out.println("Enter second term: ");
+        try (Scanner reader = new Scanner(System.in)) {
+            System.out.println("Enter number of term: ");
+            System.out.println("Enter first term: ");
+            System.out.println("Enter second term: ");
 
-        int n = reader.nextInt();
-        int firstTerm = reader.nextInt();
-        int secondTerm = reader.nextInt();
-        System.out.println("Fibonacci Series till " + n + " terms:");
+            int n = reader.nextInt();
+            int firstTerm = reader.nextInt();
+            int secondTerm = reader.nextInt();
+            System.out.println("Fibonacci Series till " + n + " terms:");
 
-        for (int i = 1; i <= n; ++i) {
-            System.out.print(firstTerm + ", ");
+            for (int i = 1; i <= n; ++i) {
+                System.out.print(firstTerm + ", ");
 
-            int nextTerm = firstTerm + secondTerm;
-            firstTerm = secondTerm;
-            secondTerm = nextTerm;
+                int nextTerm = firstTerm + secondTerm;
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+            }
         }
     }
 }
