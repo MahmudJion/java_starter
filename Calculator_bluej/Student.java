81c108607876ed4cafee67c3a591f16355f39a68
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-01 07:13:52 +0000

    add Student.java

---
+++ Calculator_bluej/Student.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Student here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Student
+{
+    String name;
+    public static int totalStudent;
+}
