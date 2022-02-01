0d110b545c7b9eddddf58205d4a12967a0a97d14
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-01 22:10:02 +0000

    update passed_failed.java

---
+++ passed_failed.java
@@ -0,0 +1,12 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        int mark = 56;
+        if(mark >= 50)
+        System.out.println("The student has passed.");
+        else
+        System.out.println("The student has failed.");
+
+    }
+}
