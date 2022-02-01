5497b904806669c07f4b17ecd57a6d160e0eedfd
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-01 07:13:40 +0000

    add Calculator.java

---
+++ Calculator_bluej/Calculator.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Calculator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Calculator
+{
+    int firstNumber;
+    float SecondNumber;
+    
+    public int add(int x, int y)
+    {
+        int sum = x + y;
+        return sum;
+    }
+
+
+}
