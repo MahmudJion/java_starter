af2b94690908fa1db4aa70ca24da13d72e28edfe
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-01 07:14:29 +0000

    RunMe added

---
+++ Calculator_bluej/RunMe.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class RunMe here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class RunMe
+{
+    public static void main(String[] abc)
+    {
+        System.out.println("\f");
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter first Number: ");
+        System.out.println("Enter Second Number: ");
+        
+        int x= reader.nextInt();
+        //get the firstNumber from the user
+        int y= reader.nextInt();
+        //get the secondNumber from the user
+        
+        Calculator c1 = new Calculator();
+        int z = c1.add(x, y);
+        System.out.println("Total Number is " + z);
+
+        
+    }
+}
