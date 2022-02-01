a7dd2d20c2f9d880d7f66e5adecf390aee1523ba
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-01 22:21:30 +0000

    fix reader issue

--- passed_failed.java
+++ passed_failed.java
@@ -4,14 +4,15 @@ public class passed_failed {
 
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Enter number:48");
+        try (Scanner reader = new Scanner(System.in)) {
+            System.out.println("Enter number:48");
 
-        int mark = reader.nextInt();
-        if(mark >= 50)
-        System.out.println("The student has passed.");
-        else
-        System.out.println("The student has failed.");
+            int mark = reader.nextInt();
+            if(mark >= 50)
+            System.out.println("The student has passed.");
+            else
+            System.out.println("The student has failed.");
+        }
 
     }
 }
