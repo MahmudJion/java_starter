1eece4c18aff0ddcbedffbcb645676c719e347c7
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-03 07:41:48 +0000

    update println options

--- ProductInfo.java
+++ ProductInfo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ProductInfo {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Please enter the name: ");
+            System.out.println("Please enter the name:");
             String name = scanner.nextLine();
 
             System.out.println("Please enter the price:");
