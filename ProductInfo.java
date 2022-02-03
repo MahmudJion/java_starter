dfafff5e49ef92825de6e63f636492b01864b8d9
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-03 07:40:00 +0000

    add ProudInfo.java

---
+++ ProductInfo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ProductInfo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the name");
+        String name = scanner.nextLine();
+
+        System.out.println("Please enter the price");
+        float price = scanner.nextFloat();
+
+        System.out.println("Please enter the quantity");
+        int quantity = scanner.nextInt();
+
+        System.out.println("Product Name : " +name);
+        float total = (float) (price * quantity * 1.2);
+        System.out.printf("Total cost: ¡ê%.2f %n", total);
+
+    }
+}
