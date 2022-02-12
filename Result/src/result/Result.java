4cd08aab5d3293c78343e7fea1bb82835e843c21
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-12 22:46:20 +0000

    add Result.java

---
+++ Result/src/result/Result.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package result;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Result {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try (Scanner reader = new Scanner(System.in)) {
+            System.out.println("Enter number:");
+
+            int mark = reader.nextInt();
+            if(mark >= 50)
+            System.out.println("The student has passed.");
+            else
+            System.out.println("The student has failed.");
+        }
+    }
+    
+}
