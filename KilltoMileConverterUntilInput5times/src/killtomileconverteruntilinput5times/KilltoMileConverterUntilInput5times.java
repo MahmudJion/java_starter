e0550ca10aedda4ca8d21218155e9d356de1f223
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-25 08:26:42 +0000

    update KilltoMileConverterUntilInput5times

--- KilltoMileConverterUntilInput5times/src/killtomileconverteruntilinput5times/KilltoMileConverterUntilInput5times.java
+++ KilltoMileConverterUntilInput5times/src/killtomileconverteruntilinput5times/KilltoMileConverterUntilInput5times.java
@@ -9,13 +9,24 @@ package killtomileconverteruntilinput5times;
  *
  * @author lenovo
  */
+import java.util.Scanner;
+
 public class KilltoMileConverterUntilInput5times {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner console = new Scanner(System.in);
+        
+        int counter = 0;
+        while (counter < 5) {
+            System.out.println("Please enter the mileage");
+            float mileage = console.nextFloat();
+            float kilo = mileage * 8 / 5;
+            System.out.printf("Kilometers : %.2f %n", kilo);
+        }
     }
-    
+
 }
+
