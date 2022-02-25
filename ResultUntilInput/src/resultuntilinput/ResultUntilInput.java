239e9480c7f4fcf385ca55a5cca7435b0605c761
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-25 08:20:59 +0000

    update user input method till -1

--- ResultUntilInput/src/resultuntilinput/ResultUntilInput.java
+++ ResultUntilInput/src/resultuntilinput/ResultUntilInput.java
@@ -9,13 +9,25 @@ package resultuntilinput;
  *
  * @author lenovo
  */
+import java.util.Scanner;
+
 public class ResultUntilInput {
 
     /**
      * @param args the command line arguments
      */
+    static Scanner console = new Scanner(System.in);
+
     public static void main(String[] args) {
         // TODO code application logic here
+        int mark;
+
+        System.out.println("Please enter your mark");
+        mark = console.nextInt();
+        while (mark != -1) {
+            mark = console.nextInt();
+        }
+        System.out.println("You have entered -1, brilliant");
     }
-    
+
 }
