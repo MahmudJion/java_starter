d5e872b251f5fdee2e39e0bfb87dc57afcd1b418
MahmudJion <mahmudh.jion02@gmail.com>
2022-02-25 08:28:22 +0000

    update package src

--- KilltoMileConverterUntilInput5times/src/killtomileconverteruntilinput5times/KilltoMileConverterUntilInput5times.java
+++ KilltoMileConverterUntilInput5times/src/killtomileconverteruntilinput5times/KilltoMileConverterUntilInput5times.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package killtomileconverteruntilinput5times;
+package KilltoMileConverterUntilInput5times.src.killtomileconverteruntilinput5times;
 
 /**
  *
@@ -17,14 +17,14 @@ public class KilltoMileConverterUntilInput5times {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        
-        int counter = 0;
-        while (counter < 5) {
-            System.out.println("Please enter the mileage");
-            float mileage = console.nextFloat();
-            float kilo = mileage * 8 / 5;
-            System.out.printf("Kilometers : %.2f %n", kilo);
+        try (Scanner console = new Scanner(System.in)) {
+            int counter = 0;
+            while (counter < 5) {
+                System.out.println("Please enter the mileage");
+                float mileage = console.nextFloat();
+                float kilo = mileage * 8 / 5;
+                System.out.printf("Kilometers : %.2f %n", kilo);
+            }
         }
     }
 
