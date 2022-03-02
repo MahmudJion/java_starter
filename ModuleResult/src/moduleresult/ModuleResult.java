67f4d87be823f36b1c4543a27763f4d5db11e2d1
MahmudJion <mahmudh.jion02@gmail.com>
2022-03-02 10:37:06 +0000

    update ModuleResult.java

--- ModuleResult/src/moduleresult/ModuleResult.java
+++ ModuleResult/src/moduleresult/ModuleResult.java
@@ -5,6 +5,7 @@
  */
 package moduleresult;
 
+import java.util.Scanner;
 /**
  *
  * @author lenovo
@@ -14,8 +15,25 @@ public class ModuleResult {
     /**
      * @param args the command line arguments
      */
+    static Scanner console = new Scanner(System.in);
+
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Please enter your mark");
+        int mark = console.nextInt();
+        if (moduleResult(mark)) {
+            System.out.println("The student has passed.");
+        } else {
+            System.out.println("The student has failed.");
+        }
     }
-    
+
+    static boolean moduleResult(int studentMark) {
+        if (studentMark >= 50) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
