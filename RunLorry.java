a512837440cd41348d1a9b6df96048ea604aaa0f
MahmudJion <mahmudh.jion02@gmail.com>
2022-03-04 10:13:04 +0000

    add RunLorry.java

---
+++ RunLorry.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class RunLorry here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RunLorry
+{
+    public static void main(String[] args)
+    {
+        Lorry myLorry = new Lorry();
+        myLorry.draw();
+    }
+}
+
+
+    
