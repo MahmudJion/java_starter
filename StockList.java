92c02efe36b30bac607ba7b809332b4dfb721067
MahmudJion <mahmudh.jion02@gmail.com>
2022-03-09 13:23:03 +0000

    add StockList.java

---
+++ StockList.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hc18aau
+ */
+import java.util.ArrayList;
+public class StockList {
+    
+    ArrayList<StockItem> warehouse; 
+    public StockList()
+    {
+        warehouse = new ArrayList<StockItem>();
+    }
+    
+    public void addStockItem(StockItem stoItem)
+    {
+        warehouse.add(stoItem);
+    }
+    
+    public int noOfStockItems()
+    {
+        return warehouse.size();
+    }
+    
+    public void removeItem(int index)
+    {
+        if(0<=index && index<warehouse.size())
+            warehouse.remove(index);
+    }
+    
+    public void showAllItems()
+    {
+        for(int index=0;index<warehouse.size();index++)        
+            System.out.println(warehouse.get(index).toString());     
+    }
+    
+    public String getAllItems()
+    {
+        String s = "";
+        int index = 0;
+        while (index < warehouse.size())
+        { 
+            s = s + warehouse.get(index).toString() +"\n";
+            //OR  in one line of code ??
+            index++;
+        }
+        return s;
+    }
+    
+    public StockItem getItem(int index)
+    {
+        if(0<=index && index<warehouse.size())
+            return warehouse.get(index);     
+        
+        return null;
+    }
+    
+    public StockItem getItem(String descrp)
+    {
+        for(int index=0; index<warehouse.size(); index++)
+        { 
+            if(warehouse.get(index).getDescription().equalsIgnoreCase(descrp)) 
+                return warehouse.get(index);
+        }
+        return null;
+    }
+    
+    public StockItem createNewStockItem(String desc, double pri, int lev)
+    {
+        StockItem stoItem = new StockItem(desc, pri, lev);
+        return stoItem;
+    }
+}
