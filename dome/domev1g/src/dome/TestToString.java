8c52e9c359d3d4925c4dedc2171e0c12daa232f5
MahmudJion <mahmudh.jion02@gmail.com>
2022-03-22 09:06:12 +0000

    add domev1g

---
+++ dome/domev1g/src/dome/TestToString.java
@@ -0,0 +1,30 @@
+package dome;
+/**
+ * Testing toString()
+ */
+public class TestToString
+{
+    public static void main(String[] args)
+    {   
+        Item p = new Item("AAA", 20);
+        DVD v = new DVD("Matilda","Disney",94);
+        CD c = new CD("Metallica", "Metallica",9,75);
+        
+        System.out.println("***** using println not toString *****");
+        System.out.println("***print Item***");
+        System.out.println(p);
+        System.out.println("***print DVD***");
+        System.out.println(c);
+        System.out.println("***print CD***");
+        System.out.println(v);
+        
+        System.out.println("\n\n***** using toString() *****");
+        System.out.println("***print Item***");
+        System.out.println(p.toString());
+        System.out.println("***print DVD***");
+        System.out.println(c.toString());
+        System.out.println("***print CD***");
+        System.out.println(v.toString());
+        
+    }
+}
