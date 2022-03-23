64981dccd41e44eca765b319d6ce15b6bdfd47f4
MahmudJion <mahmudh.jion02@gmail.com>
2022-03-23 07:08:36 +0000

    add domev1h

---
+++ domev1h/src/dome/DBTester.java
@@ -0,0 +1,36 @@
+package dome;
+/**
+ * Write a description of class Tester here.
+ * 
+ * @author A.A.Marczyk 
+ * @version 2017.10.18
+ */
+public class DBTester
+{
+    public static void main(String args[])
+    {
+        Database db = new Database();
+        
+        CD cd1 = new CD("Funky Music", "Joe Bloggs", 10, 80);
+        DVD dvd1 = new DVD("Psycho","Hitchcock",160);
+        Item it1 = new Item("XXX",100);
+        
+        //Polymorphic creation
+        Item cd2 = new CD("Boring Music", "Fred Bloggs", 7, 80);
+        Item dvd2 = new DVD("Bambi","Disney",100);
+        
+        // Won't compile ??
+        CD cd3 = new Item("Metallica",100);
+        DVD dvd3 = new Item("Alien",150);
+        CD cd3 = new DVD("Metallica","Metallica", 100);
+        
+        // non-polymorphic method call
+        db.addCD(cd1);
+        db.addDVD(dvd1);
+        
+        db.addCD(cd2);        
+        db.addDVD(dvd2);
+        
+        db.list();
+    }
+}
