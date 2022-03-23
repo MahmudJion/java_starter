5f6b8d0d5eaaf0bdd3dc53c3136f85ac2895988b
MahmudJion <mahmudh.jion02@gmail.com>
2022-03-23 07:41:34 +0000

    add zuulnb src dir

---
+++ zuulnb/zuul/src/zuulnb/Room.java
@@ -0,0 +1,63 @@
+package zuulnb;
+/**
+ * Class Room - a room in an adventure game.
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * A "Room" represents one location in the scenery of the game.  It is 
+ * connected to other rooms via exits.  The exits are labelled north, 
+ * east, south, west.  For each direction, the room stores a reference
+ * to the neighboring room, or null if there is no exit in that direction.
+ * 
+ * @author  Michael Kolling and David J. Barnes
+ * @version 2006.03.30
+ */
+public class Room 
+{
+    public String description;
+    public Room northExitRoom;
+    public Room eastExitRoom;
+    public Room southExitRoom;
+    public Room westExitRoom;
+
+    /**
+     * Create a room described "description". Initially, it has
+     * no exits. "description" is something like "a kitchen" or
+     * "an open court yard".
+     * @param description The room's description.
+     */
+    public Room(String description) 
+    {
+        this.description = description;
+    }
+
+    /**
+     * Define the exits of this room.  Every direction either leads
+     * to another room or is null (no exit there).
+     * @param north The north exit.
+     * @param east The east east.
+     * @param south The south exit.
+     * @param west The west exit.
+     */
+    public void setExitRooms(Room north, Room east, Room south, Room west) 
+    {
+        if(north != null)
+            northExitRoom = north;
+        if(east != null)
+            eastExitRoom = east;
+        if(south != null)
+            southExitRoom = south;
+        if(west != null)
+            westExitRoom = west;
+    }
+
+    /**
+     * @return The description of the room.
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+}
