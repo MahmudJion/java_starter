9984e801513c931e7c10b9f19bb76bf0f91028c0
MahmudJion <mahmudh.jion02@gmail.com>
2022-03-30 07:59:38 +0100

    Soutions for 02a flightDP

---
+++ flights/Solutions in BlueJ/02a flightDP-solution/Agency.java
@@ -0,0 +1,72 @@
+import java.util.*;
+/**
+ * Class Agency explores approaches to error handling.
+ * @author  
+ * @version 1 - Error Avoidance(Programming by Contract)
+ */
+public class Agency
+{
+    private ArrayList<Flight> flights = new ArrayList<Flight>();
+
+    /** adds a flight to the flights at the agency
+     * @param flight is the flight to be added
+     */
+    public void addFlight (Flight flight) 
+    {
+        flights.add (flight);
+    }
+    
+    /**
+     * Add a Passenger to a Flight
+     * @param pp the Passenger to be added
+     * @param flt the Flight to which the passenger is to be added
+     */
+    public void bookFlight (Flight flt,Passenger pp)
+    {
+        if(isFlight(flt) && flt.hasPlaces())
+        {
+            flt.addPassenger(pp);
+        }
+    }
+    
+    
+    /**
+     * remove a Passenger from a Flight
+     * @param pp the Passenger to be added
+     * @param flt the Flight to which the passenger is to be added
+     */
+    public void cancelBooking (Flight flt, Passenger pp)
+    {
+        if(isFlight(flt) && flt.isPassenger(pp))
+        {
+            flt.removePassenger (pp);
+        }
+    }
+    
+    
+    
+    /** returns true if the flight is with the Agency, else false
+     * @param the flight
+     * @return true if the flight is with the Agency, else false
+     */    
+    public boolean isFlight(Flight fl)
+    {
+        return flights.contains(fl);
+    }
+     
+    
+    /** provides a String representation of all flights at the agency
+     * @return a String representation of all flights at the agency
+     */
+    public String listFlights()
+    {
+        String s = "";
+        for (Flight temp : flights)
+        {
+            s = s + temp.toString()+ "\n";
+        }
+        return s; 
+    }
+    
+     
+}
