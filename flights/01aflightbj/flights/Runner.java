ec60f951c26f47f22d83148d7f17bb15a11f9533
MahmudJion <mahmudh.jion02@gmail.com>
2022-03-30 07:56:46 +0100

    add flights 01aflightbj dir

---
+++ flights/01aflightbj/flights/Runner.java
@@ -0,0 +1,16 @@
+package flights;
+
+
+/**
+ * Write a description of class Runner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        new Tester().doTest();
+    }
+}
