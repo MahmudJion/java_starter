61c72ffe162706e29429853026caa101d7430e19
MahmudJion <mahmudh.jion02@gmail.com>
2022-03-30 08:01:29 +0100

    Solutions for 02d flightsDP-string

---
+++ flights/Solutions in BlueJ/02d flightDP-string/Agency.java
@@ -0,0 +1,86 @@
+import java.util.*;
+/**
+ * Class Agency explores approaches to error handling.
+ * @author  
+ * @version 1 - Error Avoidance(Programming by Contract)
+ */
+public class Agency
+{
+    private ArrayList<Flight> flights = new ArrayList<Flight>();
+
+    /** adds a flight to the flights at the agency
+     * @param flight is the flight to be added
+     */
+    public void addFlight (Flight flight) 
+    {
+        flights.add (flight);
+    }
+    
+    /**
+     * Add a Passenger to a Flight
+     * @param pp the Passenger to be added
+     * @param flt the Flight to which the passenger is to be added
+     */
+    public String bookFlight (Flight flt,Passenger pp)
+    {
+        if(!isFlight(flt))
+        {
+            return flt.getCode() + " not a flight at the Agency";
+        }
+        else if (!flt.hasPlaces())
+        {
+            return flt.getCode() + " is FULL";
+        }
+        else
+        {
+            flt.addPassenger(pp);
+            return flt.getCode() + " booked for " + pp.getName();
+        }
+    }
+ 
+    
+    /**
+     * remove a Passenger from a Flight
+     * @param pp the Passenger to be added
+     * @param flt the Flight to which the passenger is to be added
+     */
+    public boolean cancelBooking (Flight flt, Passenger pp)
+    {
+        if(isFlight(flt) && flt.isPassenger(pp))
+        {
+            flt.removePassenger (pp);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    
+    
+    /** returns true if the flight is with the Agency, else false
+     * @param the flight
+     * @return true if the flight is with the Agency, else false
+     */    
+    public boolean isFlight(Flight fl)
+    {
+        return flights.contains(fl);
+    }
+     
+    
+    /** provides a String representation of all flights at the agency
+     * @return a String representation of all flights at the agency
+     */
+    public String listFlights()
+    {
+        String s = "";
+        for (Flight temp : flights)
+        {
+            s = s + temp.toString()+ "\n";
+        }
+        return s; 
+    }
+    
+     
+}
