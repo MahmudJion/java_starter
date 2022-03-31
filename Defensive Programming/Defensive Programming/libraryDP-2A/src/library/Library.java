ce6fc20daa6e3b86e9c8d08a5b7986afccf62e06
MahmudJion <mahmudh.jion02@gmail.com>
2022-03-31 07:33:18 +0100

    add libraryDP-2A to the Defensive Programming dir

---
+++ Defensive Programming/Defensive Programming/libraryDP-2A/src/library/Library.java
@@ -0,0 +1,94 @@
+package library;
+import java.util.*;
+/**
+ * Class Library explores approaches to error handling.
+ * 
+ * @author Mick Wood, amended A.Marczyk 
+ * @version 2 - Defensive Programming with Error Codes
+ * Based on Library 1b
+ */
+public class Library
+{
+    private ArrayList<Member> members = new ArrayList<Member>();
+    private ArrayList<Book> books = new ArrayList<Book> ();
+    
+    public void addMember (Member member) {members.add (member);}
+    public void addBook (Book book) {books.add (book);}
+
+    /**
+     * Loan a Book to a Member
+     * In-built defenses, but what to do if conditions not met ?
+     * pre-condition : NONE
+     * @param b the Book to be loaned
+     * @param m the Member to loan the Book
+     */
+    public void lendBook (Member m,Book b)
+    {
+        if (b.isAvailable() && isMember(m) && isBook(b) && !m.hasFines())
+        {            
+            m.borrowBook (b);
+            b.setUnavailable();
+        }
+        else 
+        {
+            // do nothing
+        }            
+    }
+    
+    /** return book
+     * PRECONDITION: isOnLoan(Member m, Book b)
+     */
+    public void returnBook (Member m, Book b)
+    {
+        m.returnBook (b);
+        b.setAvailable();
+    }
+
+    public boolean isOnLoan(Member m, Book b)
+    {
+        return m.hasBook(b);
+    }
+    
+    public boolean isMember(Member m)
+    {
+        return members.contains(m);
+    }
+    
+    public boolean isBook(Book b)
+    {
+        return books.contains(b);
+    }
+    
+    public String listAvailableBooks()
+    {
+        String s = "";
+        for (Book temp : books)
+        {
+            if (temp.isAvailable())
+            {
+                s = s + temp.getTitle()+ "\n";
+            }
+        }
+        return s;
+    }
+    
+    public String listMembers()
+    {
+        String s = "";
+        for (Member temp : members)
+        {
+            s = s + temp.getName()+ "\n";
+        }
+        return s; 
+    }
+    
+    public String listAllBooks()
+    {
+        String s = "";
+        for (Book temp : books)
+        {
+            s = s + temp.getTitle()+ "\n";
+        }
+        return s; 
+    }
+}
---
+++ Defensive Programming/Defensive Programming/libraryDP-2A/src/library/Tester.java
@@ -0,0 +1,81 @@
+package library;
+/**
+ * Client Class for Library example.
+ * Explores approaches to error handling.
+ * 
+ * @author Mick Wood 
+ * @version 2 - Defensive Programming with Error Codes
+ */
+public class Tester
+{
+    private Library myLibrary = new Library();
+    
+    private Member m1,m2,m3;
+    private Book b1,b2,b3,b4,b5;
+        
+    public Tester()
+    {
+        m1 = new Member ("Fred");
+        m2 = new Member ("Amit");
+        m3 = new Member ("Olek");  // not added to myLibrary
+    
+        b1 = new Book ("Wuthering Heights");
+        b2 = new Book ("1984");
+        b3 = new Book ("Brave New World");
+        b4 = new Book ("The Book Thief");
+        b5 = new Book ("Jane Eyre"); // not added to myLibrary      
+        
+        myLibrary.addMember (m1); myLibrary.addMember (m2);
+        // m3 not added to myLibrary
+        myLibrary.addBook (b1); myLibrary.addBook (b2);
+        myLibrary.addBook (b3); myLibrary.addBook (b4);
+        
+        // b5 not added to myLibrary
+        System.out.println ("A version of Library.");
+        System.out.println("***********Members***************");
+        System.out.println(myLibrary.listMembers());
+        System.out.println("*******Available books***********");
+        System.out.println(myLibrary.listAllBooks());
+    }
+
+    
+    public void doTest ()
+    {
+        System.out.println("********** Client - No need to be responsible***********");
+        myLibrary.lendBook (m1, b1);     
+        myLibrary.lendBook (m2, b2);
+        // What happens here ?
+        // should not be able to do these !
+        myLibrary.lendBook (m2, b1);      // book already on loan
+        myLibrary.lendBook (m3, b1);      // not member of myLibrary
+        myLibrary.lendBook (m2, b5);      // book not in myLibrary
+        m2.addFine(10);
+        myLibrary.lendBook (m2, b3);
+        m2.payFine(10);
+        myLibrary.lendBook (m2, b3);
+        // In practice, the client wants to have some idea of what is happening
+        
+        System.out.println ("\n\nReturn Book");
+        //Return book - which is on loan
+        System.out.println(doReturn(m1,b1));
+
+        //Return book - which is not on loan
+        System.out.println(doReturn(m1,b4));
+    }
+    
+    private String doReturn(Member m, Book b)
+    {
+        //Return book - which is on loan
+        if (myLibrary.isOnLoan(m,b))
+        {
+            myLibrary.returnBook(m,b);
+            return b.getTitle() + " was returned by " 
+            + m.getName(); 
+        }
+        else
+        {
+            return b.getTitle() + " not on loan to " 
+            + m.getName();
+        } 
+    }
+}
