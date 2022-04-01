8b9a04b025caffcc1a9e33a1ff60844bf10dbd48
MahmudJion <mahmudh.jion02@gmail.com>
2022-04-01 07:19:30 +0100

    add zuul -with-enums-v1

---
+++ zuul-with-enums-v1/src/zuul/Runner.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zuul;
+
+/**
+ *
+ * @author comqaam
+ */
+public class Runner {
+    public static void main(String[] args) {
+        new Game().play();
+    }
+    
+}
