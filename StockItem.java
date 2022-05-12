9803866aa4fc9fd7d9ebb9a7c6372b0eb03ebcd9
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-12 08:30:49 +0100

    update StockItem.java

--- StockItem.java
+++ StockItem.java
@@ -1,26 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author hc18aau
- */
 public class StockItem {
     private String description;
     private int level;
     private double price;
     private Date sellByDate;
-    
+
     public StockItem(String desc, double pri, int lev)
     {
         this.description = desc;
         this.price = pri;
         this.level = lev;
     }
-    
+
     public StockItem(String desc, double pri, int lev, Date sByDate)
     {
         this.description = desc;
@@ -28,50 +18,50 @@ public class StockItem {
         this.level = lev;
         this.sellByDate = sByDate;
     }
-    
+
     public void printReport()
     {
         System.out.println("*******Stock Report *******");
         System.out.println("   Item  :  " + this.description);
         System.out.printf("   Price: %.2fp%n", this.price);
         System.out.printf("  Stock level  : %d%n ", this.level);
-        //System.out.println(" Date :" + this.sellByDate.getAsString());    
+        //System.out.println(" Date :" + this.sellByDate.getAsString());
     }
-    
+
     @Override
     public String toString()
     {
 	return "*******Stock Report ******* \n Item: " +this.description+ "\n Price: " +this.price + "\n Stock level : " +this.level;
     }
-    
+
     public void setPrice(double individualPrice)
     {
         this.price = individualPrice;
     }
-    
+
     public void doDelivery(int amount)
     {
         this.level += amount;
         //return this.level;
     }
-    
+
     public void doSale(int amount)
     {
-        if(this.level >= amount)     
+        if(this.level >= amount)
            this.level -= amount;
         //return this.level;
     }
-    
+
     public int getStockLevel()
     {
         return this.level;
     }
-    
+
     public String getDescription()
     {
         return this.description;
     }
-    
+
     public void setSellByDate(int d, int m, int y)
     {
        this.sellByDate.setDate(d, m, y);
