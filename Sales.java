932e36d0beb4bbf2575b4cfa9bddea4862039b21
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-12 08:30:18 +0100

    update Sales.java

--- Sales.java
+++ Sales.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author hc18aau
- */
 import java.util.Scanner;
 
 public class Sales {
-    
+
     public static void main(String[] args)
     {
         Scanner reader = new Scanner(System.in);
