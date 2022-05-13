5f5fc73a6dca0147644c621e1d331ad5d8f830b8
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-13 07:25:53 +0100

    update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,14 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author hc18aau
- */
-
 public class Tester {
     public static void main(String[] args)
     {
@@ -24,8 +13,8 @@ public class Tester {
         bob.doDelivery(10);
         //System.out.println(bob.toString());
         bob.doSale(8);
-        
-       // System.out.println(bob.toString());
+
+        //System.out.println(bob.toString());
         System.out.println(bob.getStockLevel());
         System.out.println(bob.getDescription());
         System.out.println("done");
