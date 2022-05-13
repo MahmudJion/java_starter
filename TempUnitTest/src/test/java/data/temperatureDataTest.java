538617291ac6334abdb9e7f206aa27f7bb85ac15
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-13 07:30:49 +0100

    fixed and removed unused imports

--- TempUnitTest/src/test/java/data/temperatureDataTest.java
+++ TempUnitTest/src/test/java/data/temperatureDataTest.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -17,45 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author victorsteven
  */
 public class temperatureDataTest {
-    
+
     public temperatureDataTest() {
     }
-
-    @org.junit.jupiter.api.BeforeAll
-    public static void setUpClass() throws Exception {
-    }
-
-    @org.junit.jupiter.api.AfterAll
-    public static void tearDownClass() throws Exception {
-    }
-
-    @org.junit.jupiter.api.BeforeEach
-    public void setUp() throws Exception {
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    public void tearDown() throws Exception {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    /**
-     *
-     */
-    @AfterEach
-    public void tearDown() {
-    }
-
     /**
      * Test of getTemperature method, of class temperatureData.
      */
@@ -136,5 +90,5 @@ public class temperatureDataTest {
         // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
     }
-    
+
 }
