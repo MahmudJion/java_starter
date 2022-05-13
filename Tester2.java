9515dacbcfe2ae37a2c8916a131f82b34725ca6a
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-13 07:26:26 +0100

    update Tester2.java

--- Tester2.java
+++ Tester2.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author hc18aau
- */
 public class Tester2 {
     public static void main(String[] args)
     {
@@ -20,6 +10,6 @@ public class Tester2 {
         product1.printReport();
         product2.printReport();
         product3.printReport();
-        
+
     }
 }
