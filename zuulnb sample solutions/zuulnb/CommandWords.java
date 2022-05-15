1fc3f146b50855544e09a3189cb859a3fca81c74
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-15 10:39:19 +0100

    update CommandWords.java

--- zuulnb sample solutions/zuulnb/CommandWords.java
+++ zuulnb sample solutions/zuulnb/CommandWords.java
@@ -1,8 +1,8 @@
 package zuulnb;
 /**
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
- * 
+ * This class is part of the "World of Zuul" application.
+ * "World of Zuul" is a very simple, text based adventure game.
+ *
  * This class holds an enumeration of all command words known to the game.
  * It is used to recognise commands as they are typed in.
  *
@@ -26,7 +26,7 @@ public class CommandWords
     }
 
     /**
-     * Check whether a given String is a valid command word. 
+     * Check whether a given String is a valid command word.
      * @return true if a given string is a valid command,
      * false if it isn't.
      */
