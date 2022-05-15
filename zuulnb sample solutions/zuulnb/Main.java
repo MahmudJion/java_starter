964d82800373ab2b960811c6655d11455ca588c1
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-15 10:39:44 +0100

    update main java class

--- zuulnb sample solutions/zuulnb/Main.java
+++ zuulnb sample solutions/zuulnb/Main.java
@@ -1,8 +1,8 @@
 package zuulnb;
 /**
  * Write a description of class Main here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 public class Main
