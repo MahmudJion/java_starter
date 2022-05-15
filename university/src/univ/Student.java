b8bb7241967342f26d25596e17abf0ff9424d4fd
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-15 10:44:27 +0100

    update Student.java

--- university/src/univ/Student.java
+++ university/src/univ/Student.java
@@ -1,78 +1,77 @@
-
 package univ;
 /**
  * The Student class stores information about a student  at the University
  * Students are enrolled on only one course called a "pathway" e.g. "CS2", "CSP4"
  * For simplicity, in this version the pathway is just stored as a String
- * 
+ *
  */
 
-public class Student                                
+public class Student
 {
     // fields
-    private int idNumber;                           
-    private String name; 
+    private int idNumber;
+    private String name;
     private String pathway;
     private boolean international;
- 
+
     /**
     * Creates a Student with a unique id number and a name initialised from parameters
     * Students are treated as "home" students by default
     * @param id it is the responsibility of the client to ensure that id is a unique number
     * @param n is the String representation of the student's name
     * @param pway is the pathway given as a String
-    */ 
-    public Student (int id, String n, String pway)    
+    */
+    public Student (int id, String n, String pway)
     {
-        idNumber = id;                              
-        name = n; 
-        pathway = pway; 
-        international = false;                                 
+        idNumber = id;
+        name = n;
+        pathway = pway;
+        international = false;
     }
-    
+
     /**
     * returns student id as an int
     * @return value returned is student id as an int
-    */                                                       
-    public int getIdNo ()                           
+    */
+    public int getIdNo ()
     {
-        return idNumber;                           
-    }   
+        return idNumber;
+    }
 
     /**
     * returns student name as a String
     * @return value returned is student name as a String
-    */ 
+    */
     public String getName ()
     {
-        return name;                                
+        return name;
     }
-     
+
     /**
     * @return returns true if the students is an international student, else false
-    */ 
+    */
     public boolean isInternational ()
     {
-        return international;                                
-    }   
+        return international;
+    }
 
     /**
     * resets student from "Home" (default) to International
-    */ 
+    */
     public void setInternational ()
     {
-        international = true;                                
-    }   
+        international = true;
+    }
 
 
 
- 
+
     /**
     * Returns a String representation of the student
     * @return returns a String representation of the student, including the id number and name
     */
     public String toString()
-    {   
+    {
         String s;
         if (isInternational())
         {
@@ -83,18 +82,17 @@ public class Student
             s = "  Home  student ";
         }
         return (idNumber + "  " + name + "  " + pathway + "  " + s );
-    } 
+    }
 }
-    
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-                                                       
+
+
+
+
+
+
+
+
+
+
+
