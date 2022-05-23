666a12131a356e6207c5aec7b09038599e83f45a
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-23 07:43:55 +0100

    update Card.java

--- cards1-static/src/cards/Card.java
+++ cards1-static/src/cards/Card.java
@@ -1,22 +1,11 @@
 package cards;
-
-
-/**
- * Write a description of class Card here.
- * Depends on the programmer remembering the codes 
- * allocated to each type.  There is no error checking
- * or easy String versions
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Card
 {
     public static final int CLUB = 1;
     public static final int DIAMOND = 2;
     public static final int HEART = 3;
     public static final int SPADE = 4;
-    
+
     public static final int JACK = 11;
     public static final int QUEEN = 12;
     public static final int KING = 13;
@@ -26,14 +15,13 @@ public class Card
     private int faceValue;
 
     public Card(int s, int fv) //Note types are int !!
-    {  
-        suit = s; 
+    {
+        suit = s;
         faceValue = fv;
     }
- 
- // using nested if's
+
     public String getSuitName()
-    {  
+    {
         String s = "";
         if (suit == CLUB){s = "Club";}
         else if (suit == HEART){s = "Heart";}
@@ -42,20 +30,6 @@ public class Card
         return s;
     }
 
-/*
-    public String getSuitName() 
-    {  
-        String s = "";
-        switch(suit)
-        {   
-            case CLUB :   s = "Club";  break;
-            case HEART:   s = "Heart";break;
-            case DIAMOND: s = "Diamond";break;
-            case SPADE:   s = "Spade";break;
-        }
-        return s;
-    }
-*/
     public String toString()
     {
         String face = "" + faceValue;
@@ -63,8 +37,8 @@ public class Card
         else if (faceValue == 12) {face = "Queen";}
         else if (faceValue == 13) {face = "King";}
         else if (faceValue == 1)  {face = "Ace";}
-        face = face + " of " + getSuitName()+ "s";       
+        face = face + " of " + getSuitName()+ "s";
         return face;
     }
-    
+
 }
