4e82d19110298a7ebe48ba591c63c618f2f5b189
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-23 07:46:21 +0100

    update Card.java

--- cards2-enum2/src/cards/Card.java
+++ cards2-enum2/src/cards/Card.java
@@ -1,14 +1,9 @@
 package cards;
 
 import java.util.*;
-/**
- * Write a description of class Card here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Card
-{   
+{
     public static final int JACK = 11;
     public static final int QUEEN = 12;
     public static final int KING = 13;
@@ -17,26 +12,26 @@ public class Card
     private int faceValue;
 
     public Card(Suit s, int fv) //Note s is NOT int!!
-    {  
+    {
         cardSuit = s;
-        faceValue = fv;        
+        faceValue = fv;
     }
- 
-    
+
+
     public String getSuit()
-    {  
+    {
         return cardSuit.toString();
     }
-    
+
     public enum Suit  //private inner class - there could be problems with this
     {
         // A value for each suit along with its
         // corresponding user interface string.
         CLUB("Clubs"), DIAMOND("Diamonds"), HEART("Hearts"),SPADE("Spades"), UNKNOWN("Unknown");
-    
+
         // The suit.
         private String suitString;
-    
+
         /**
          * Initialise with the corresponding suit string.
          * @param suitString The suit string.
@@ -44,8 +39,8 @@ public class Card
         Suit(String suitString)
         {
             this.suitString = suitString;
-        } 
-    
+        }
+
         /**
          * @return The Suit word as a string.
          */
