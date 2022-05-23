0b6c7268e04fb056b3e5658c2f9fb957bab64414
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-23 07:39:13 +0100

    update test file

--- CarCosts/src/test/java/data/journeyCosterTest.java
+++ CarCosts/src/test/java/data/journeyCosterTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
 import org.junit.After;
@@ -17,22 +12,22 @@ import static org.junit.Assert.*;
  * @author Stiv
  */
 public class journeyCosterTest {
-    
+
     public journeyCosterTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -159,5 +154,5 @@ public class journeyCosterTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
+
 }
