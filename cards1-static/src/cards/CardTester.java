498f13dd016c12445403caab442715753a624677
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-23 07:44:06 +0100

    update CardTester.java

--- cards1-static/src/cards/CardTester.java
+++ cards1-static/src/cards/CardTester.java
@@ -1,14 +1,7 @@
 package cards;
 
-
-/**
- * Write a description of class CardTester here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class CardTester
-{   
+{
     public static void main(String[] args)
     {
       Card myCard1 = new Card(Card.CLUB, 6);
@@ -16,28 +9,8 @@ public class CardTester
       System.out.println("Card1 : " + myCard1.getSuitName());
       System.out.println("Card2 : " + myCard2.getSuitName());
       System.out.println("Card1 : " + myCard1.toString());
-      
-      //use of statics for enumerated types is not "type"- safe
-      Card myCard3 = new Card(2, 6); //what is created ? how would I know?
-      //but to associate with a particular value might be OK
-      //I would guess this is a Jack
-      Card myCard4 = new Card(Card.CLUB, 11);
-      //Use of statics means you lose the "String" representation
-      // What is output here ?
+
       System.out.println("My suit is " + Card.CLUB);
-      
-      /** this causes a syntax error - can't compare String and int
-       * replacing myString.equals(Card.CLUB) compiles but results in "No"
-      String myString = "Club";
-      if (myString==Card.CLUB)
-      {
-        System.out.println("Yes");
-      }
-      else
-      {
-        System.out.println("No");
-      }
-      */
-     
+
     }
-}   
+}
