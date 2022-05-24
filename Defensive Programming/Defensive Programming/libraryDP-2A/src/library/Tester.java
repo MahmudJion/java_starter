d63360dad5046e4067d504fe3dcba7212f1d61c9
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-24 07:30:42 +0100

    update Tester.java

--- Defensive Programming/Defensive Programming/libraryDP-2A/src/library/Tester.java
+++ Defensive Programming/Defensive Programming/libraryDP-2A/src/library/Tester.java
@@ -1,35 +1,29 @@
 package library;
-/**
- * Client Class for Library example.
- * Explores approaches to error handling.
- * 
- * @author Mick Wood 
- * @version 2 - Defensive Programming with Error Codes
- */
+
 public class Tester
 {
     private Library myLibrary = new Library();
-    
+
     private Member m1,m2,m3;
     private Book b1,b2,b3,b4,b5;
-        
+
     public Tester()
     {
         m1 = new Member ("Fred");
         m2 = new Member ("Amit");
         m3 = new Member ("Olek");  // not added to myLibrary
-    
+
         b1 = new Book ("Wuthering Heights");
         b2 = new Book ("1984");
         b3 = new Book ("Brave New World");
         b4 = new Book ("The Book Thief");
-        b5 = new Book ("Jane Eyre"); // not added to myLibrary      
-        
+        b5 = new Book ("Jane Eyre"); // not added to myLibrary
+
         myLibrary.addMember (m1); myLibrary.addMember (m2);
         // m3 not added to myLibrary
         myLibrary.addBook (b1); myLibrary.addBook (b2);
         myLibrary.addBook (b3); myLibrary.addBook (b4);
-        
+
         // b5 not added to myLibrary
         System.out.println ("A version of Library.");
         System.out.println("***********Members***************");
@@ -38,11 +32,11 @@ public class Tester
         System.out.println(myLibrary.listAllBooks());
     }
 
-    
+
     public void doTest ()
     {
         System.out.println("********** Client - No need to be responsible***********");
-        myLibrary.lendBook (m1, b1);     
+        myLibrary.lendBook (m1, b1);
         myLibrary.lendBook (m2, b2);
         // What happens here ?
         // should not be able to do these !
@@ -54,7 +48,7 @@ public class Tester
         m2.payFine(10);
         myLibrary.lendBook (m2, b3);
         // In practice, the client wants to have some idea of what is happening
-        
+
         System.out.println ("\n\nReturn Book");
         //Return book - which is on loan
         System.out.println(doReturn(m1,b1));
@@ -62,20 +56,20 @@ public class Tester
         //Return book - which is not on loan
         System.out.println(doReturn(m1,b4));
     }
-    
+
     private String doReturn(Member m, Book b)
     {
         //Return book - which is on loan
         if (myLibrary.isOnLoan(m,b))
         {
             myLibrary.returnBook(m,b);
-            return b.getTitle() + " was returned by " 
-            + m.getName(); 
+            return b.getTitle() + " was returned by "
+            + m.getName();
         }
         else
         {
-            return b.getTitle() + " not on loan to " 
+            return b.getTitle() + " not on loan to "
             + m.getName();
-        } 
+        }
     }
 }
