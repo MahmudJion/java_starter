49e8beac2de8ff21a72c82074a21684e724d4a70
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-25 07:27:34 +0100

    update Book.java

--- Defensive Programming/Defensive Programming/libraryDP-2Boolean/src/library/Book.java
+++ Defensive Programming/Defensive Programming/libraryDP-2Boolean/src/library/Book.java
@@ -3,28 +3,28 @@ public class Book
 {
     private String title;
     private boolean available;
-    
-    public Book (String title) 
-    { 
+
+    public Book (String title)
+    {
         this.title = title;
         available = true;
     }
-    
-    public String getTitle() 
+
+    public String getTitle()
     {
         return title;
     }
-    
+
     public boolean isAvailable()
     {
         return available;
     }
-    
+
     public void setUnavailable()
     {
         available  = false;
     }
-    
+
     public void setAvailable()
     {
         available  = true;
