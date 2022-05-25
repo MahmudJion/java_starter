d3493f38f06935c1264a753e10fcdbc4812a371a
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-25 07:28:58 +0100

    update Runner.java

--- Defensive Programming/Defensive Programming/libraryDP-2Codes/src/library/Runner.java
+++ Defensive Programming/Defensive Programming/libraryDP-2Codes/src/library/Runner.java
@@ -1,10 +1,4 @@
 package library;
-/**
- * Write a description of class Runner here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Runner
 {
     public static void main(String[] args)
