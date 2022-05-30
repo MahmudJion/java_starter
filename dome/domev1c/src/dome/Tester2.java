a9764cc1763a6e4d41adf2af0a8c68152aca729c
MahmudJion <mahmudh.jion02@gmail.com>
2022-05-30 07:03:44 +0100

    update Tester2.java

--- dome/domev1c/src/dome/Tester2.java
+++ dome/domev1c/src/dome/Tester2.java
@@ -1,33 +1,27 @@
 package dome;
-/* which statements will compile ?
-*/
 
 public class Tester2
-{    
+{
     public static void main(String[] args)
-    { 
+    {
         Item p = new Item("AAA", 20);
         DVD v = new DVD("Matilda","Disney",94);
-        CD c = new CD("Metallica", "Metallica",9,75); 
-        
-        String s;     
+        CD c = new CD("Metallica", "Metallica",9,75);
+
+        String s;
         // Which of these would cause compilation errors ?
         s = v.getDirector();
-        //s = c.getDirector();
-        //s = p.getDirector();
-        
-        //s =  v.getArtist();
+
         s =  c.getArtist();
-        //s =  p.getArtist();
-        
+
         s = v.getComment();
         s = c.getComment();
         s = p.getComment();
-        
+
         s = v.getTitle();
         s = c.getTitle();
         s = p.getTitle();
-        
+
 
     }
 }
