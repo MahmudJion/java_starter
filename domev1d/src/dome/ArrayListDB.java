97c9cf46308e767ac2a1ce655b34d6c683e2afef
MahmudJion <mahmudh.jion02@gmail.com>
2022-06-01 10:44:33 +0100

    update ArrayListDB.java

--- domev1d/src/dome/ArrayListDB.java
+++ domev1d/src/dome/ArrayListDB.java
@@ -1,11 +1,6 @@
 package dome;
 import java.util.*;
-/**
- * Write a description of class ArrayListDB here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class ArrayListDB
 {
     public static void main(String[] args)
@@ -13,39 +8,39 @@ public class ArrayListDB
         DVD v = new DVD("Matilda","Disney",94);
         CD c = new CD("Metallica", "Metallica",9,75);
         Item p = new Item("xxx",  100);
-        Student s = new Student("Fred",123); 
+        Student s = new Student("Fred",123);
         Item pv1;
-        
+
         ArrayList<Item> myStuff = new ArrayList<Item>();
-       
+
         // all types allowed to be stored in ArrayList
-        myStuff.add(s);     
+        myStuff.add(s);
         myStuff.add(p);     //p is Item object
         myStuff.add(v);     //v is DVD object
         myStuff.add(c);     //c is CD object
-        
+
         for(Item temp : myStuff)
         {  // call supertype method
-            temp.setOwn(true); 
+            temp.setOwn(true);
         }
-        
+
         for(Item temp : myStuff)
         {  // call supertype method
             temp.print();          //overridden method
             System.out.println("========================");
         }
         //pv1 static type Item, current dynamic type CD
-        
+
         pv1 = v;            // allowed by polymorphism
-        myStuff.add(pv1);   
+        myStuff.add(pv1);
         pv1 = c;            // allowed by polymorphism
-        myStuff.add(pv1);    // current dynamic type DVD 
+        myStuff.add(pv1);    // current dynamic type DVD
         System.out.println("**** Print with additions *****");
         for(Item temp : myStuff)
         {  // call supertype method
             temp.print();          //overridden method
             System.out.println("========================");
-        }        
-    }    
+        }
+    }
 
 }
