ae01e09d0bd6252ade57bd59c8a81d701e166122
MahmudJion <mahmudh.jion02@gmail.com>
2022-06-01 10:45:21 +0100

    update Student.java

--- domev1d/src/dome/Student.java
+++ domev1d/src/dome/Student.java
@@ -1,10 +1,5 @@
 package dome;
-/**
- * Simple student with a name and id number. Student also has credits band fees to pay
- * 
- * @author A.A.Marczyk
- * @version 18/11/2004 version 1
- */
+
 public class Student
 {
     private String name;
@@ -12,7 +7,7 @@ public class Student
     private int credits;
     private double fees;
     private double feesPaid;
-   
+
     public Student(String n, int id)
     {
        name = n;
@@ -21,48 +16,48 @@ public class Student
        fees = 0;
        feesPaid = 0;
     }
-   
+
 	public int getStudentNo()
 	{
 	    return idNo;
 	}
-	
+
 	public void addCredits(int c)
 	{
 	   credits = credits + c;
 	}
-	
+
 	public int getCredits()
 	{
 	    return credits;
 	}
-	
+
 	public void setFees(double f)
 	{
 	    fees = f;
 	}
-	
+
 	public void payFees(double p)
 	{
 	    feesPaid = feesPaid + p;
 	}
-	
+
 	public boolean hasPaid()
 	{
 	    return fees == feesPaid;
 	}
-	
+
 	public double getFeesOutstanding()
 	{
 	    return fees - feesPaid;
 	}
-	
+
 	public String getAsString()
 	{
 	    String s = "Student Name: " + name + "   Student No: " + idNo + "\nCredits :" + credits;
 	    s = s + "\nFees: �" + fees + "  Fees paid: �" + feesPaid;
 	    return s;
 	}
-	
-	
+
+
 }
