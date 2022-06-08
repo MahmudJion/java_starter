061f711a482ab0424891bdfbde9c0bff04519cf3
MahmudJion <mahmudh.jion02@gmail.com>
2022-06-08 07:00:16 +0100

    update TestPoly1.java

--- domev1h/src/dome/TestPoly1.java
+++ domev1h/src/dome/TestPoly1.java
@@ -1,9 +1,5 @@
 package dome;
-/** Tests polymorphic creation and assignment
- * Expect compilation errors
- * @author A.A.Marczyk 
- * @version 14/10/2010
- */
+
 public class TestPoly1
 {
     public static void main(String[] args)
@@ -15,53 +11,53 @@ public class TestPoly1
 
         //Polymorphic creation
         Item pv2 = new DVD("BFG","Disney",100);
-        Item pc2 = new CD("The Wall","Pink Floyd",8,70); 
+        Item pc2 = new CD("The Wall","Pink Floyd",8,70);
 
         DVD vi3 = new Item("xxx",  100);
         CD ci3 = new Item("xxx",  100);
         DVD vc4 = new CD("Metallica","Metallica",9,75);
- 
+
         Item p1, p2;
         DVD v1;
         CD c1;
-    
-        // Polymorphic assignment 
-        p1 = p ;        
-        v1 = v ;        
-        c1 = c; 
-        
-        p1 = v;         
-        p2 = c;         
 
-        v1 = p;       
-        c1 = p;       
-        v1 = c;       
+        // Polymorphic assignment
+        p1 = p ;
+        v1 = v ;
+        c1 = c;
 
-        v1 = pv2;      
+        p1 = v;
+        p2 = c;
+
+        v1 = p;
+        c1 = p;
+        v1 = c;
+
+        v1 = pv2;
         c1 = pc2;
-        
+
         // method calls - what will happen ?
         System.out.println("***print Item***");
-        p.print(); 
+        p.print();
         System.out.println("***print DVD***");
         v.print();
         System.out.println("***print CD***");
-        c.print();  
-        
+        c.print();
+
         System.out.println("***What gets printed***");
         System.out.println("***pv2 - static:Item, dynamic:DVD***");
-        pv2.print(); 
+        pv2.print();
         System.out.println("***pv2 re-assigned a CD pc2 ***");
         pv2 = pc2;
-        pv2.print(); 
+        pv2.print();
         System.out.println("***pv2 re-assigned a DVD***");
         pv2 = v;
 
- 	// Why do these method calls not compile ?  
-        // Can casting solve the problem ? 
+ 	    // Why do these method calls not compile ?
+        // Can casting solve the problem ?
         // or will there be problems at runtime ?
         String s;
-        s = pc2.getArtist();  
+        s = pc2.getArtist();
         s = pv2.getDirector();
         s = pv2.getArtist();
         s = pc2.getDirector();
