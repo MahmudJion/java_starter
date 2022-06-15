a5fed2b8969c2937d59f68f02bf9e006082a6881
MahmudJion <mahmudh.jion02@gmail.com>
2022-06-15 07:04:02 +0100

    update OneOffSwitch.java

--- event_handling1/OnOffSwitch.java
+++ event_handling1/OnOffSwitch.java
@@ -3,21 +3,21 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class OnOffSwitch {
-  
-  //all of these fields must be declared here so that the inner class can "see" 
+
+  //all of these fields must be declared here so that the inner class can "see"
   //them
   private JFrame frame;
   private JButton button;
   private String onText = "On";
   private String offText = "Off";
   private boolean isOn = false;
- 
+
   public OnOffSwitch () {
    frame = new JFrame("On/Off Switch"); // frame title
-   
+
    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   button = new JButton("Off");  
-   
+   button = new JButton("Off");
+
    button.setForeground(Color.black);
    button.setBackground(Color.white);
    button.addActionListener(new Switcher());//add the Switcher to the button
@@ -25,12 +25,12 @@ public class OnOffSwitch {
    frame.setSize(400, 400);
    frame.setVisible(true);
  }
- 
-  //here is the inner class 
+
+  //here is the inner class
   class Switcher implements ActionListener {
     //we must implement this method to use the interface ActionListener
     public void actionPerformed (ActionEvent e) {
-  
+
       Color oldForeground = button.getForeground();//we access the private fields in the enclosing class
       button.setForeground(button.getBackground());
       button.setBackground(oldForeground);
