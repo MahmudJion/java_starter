becb790083d55b074b9ec62af904f4336ace8c11
MahmudJion <mahmudh.jion02@gmail.com>
2022-06-17 21:37:13 +0100

    update Tester.java

--- flights/Solutions in BlueJ/01b flightEA-parametrise/Tester.java
+++ flights/Solutions in BlueJ/01b flightEA-parametrise/Tester.java
@@ -1,13 +1,7 @@
-
-/**
- * Tester Class is a client of Agency
- * Explores approaches to error handling. 
- * @version 1b - Error Avoidance (Programming by contract) parametrised
- */
 public class Tester
 {
     private Agency agency = new Agency();
-    private Flight f1 ,f2,f3; 
+    private Flight f1 ,f2,f3;
     private Passenger pp1, pp2, pp3, pp4, pp5;
 
     public Tester()
@@ -16,12 +10,12 @@ public class Tester
         f1 = new Flight ("RO12", "Rome", 20);
         f2 = new Flight ("AM34", "Amsterdam", 2); // for testing purposes
         f3 = new Flight ("BE45", "Berlin", 50);  // not added to Agency
-        
+
         pp1 = new Passenger ("Amy", true);
         pp2 = new Passenger ("Ben", false);
         pp3 = new Passenger ("Chen", false);
         pp4 = new Passenger ("Dan", false);
-        pp5 = new Passenger ("Eli", false); 
+        pp5 = new Passenger ("Eli", false);
         System.out.println ("A version of Agency.");
         //Add flights to the agency
         agency.addFlight (f1); agency.addFlight (f2);
@@ -30,9 +24,9 @@ public class Tester
         System.out.println(agency.listFlights());   // you can comment this out
     }
 
-    
+
     public void doTest ()
-    {     
+    {
         System.out.println("********** Responsible Client ***********");
         // write your code to check pre-conditions before making bookings
         /** Responsible client: Test precondition on bookFlight **/
@@ -40,9 +34,9 @@ public class Tester
         System.out.println(processBooking (f1,pp2));
         System.out.println(processBooking (f2,pp1));
         System.out.println(processBooking (f2,pp3));
-        System.out.println(processBooking (f3,pp1));        
-        System.out.println(processBooking (f2,pp4));        
-         
+        System.out.println(processBooking (f3,pp1));
+        System.out.println(processBooking (f2,pp4));
+
         System.out.println("\nShow all flights");
         System.out.println(agency.listFlights());
 
@@ -54,12 +48,12 @@ public class Tester
         System.out.println (f3.getCode() + " booked for " + pp3.getName());
         agency.bookFlight (f2,pp4);      // flight full
         System.out.println (f3.getCode() + " booked for " + pp4.getName());
-        agency.bookFlight (f2,pp5);      // flight full 
+        agency.bookFlight (f2,pp5);      // flight full
         System.out.println (f3.getCode() + " booked for " + pp5.getName());
         System.out.println("\nShow all flights");
         System.out.println(agency.listFlights());
     }
-    
+
     private String processBooking(Flight f, Passenger p)
     {
         if (!agency.isFlight(f))
@@ -70,7 +64,7 @@ public class Tester
         {
             return f.getCode() + " is full " ;
         }
-        else        
+        else
         {
             agency.bookFlight(f,p);
             return f.getCode() + " booked for " + p.getName();
