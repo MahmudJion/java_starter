77447f2764f42d5eaf398e4c957c3010d91e2f85
MahmudJion <mahmudh.jion02@gmail.com>
2022-06-17 21:29:34 +0100

    update Tester.java

--- flights/Solutions in BlueJ/01a flightEA-solution/Tester.java
+++ flights/Solutions in BlueJ/01a flightEA-solution/Tester.java
@@ -1,13 +1,7 @@
-
-/**
- * Tester Class is a client of Agency
- * Explores approaches to error handling. 
- * @version 1a - Error Avoidance (Programming by contract)Solution
- */
 public class Tester
 {
     private Agency agency = new Agency();
-    private Flight f1 ,f2,f3; 
+    private Flight f1 ,f2,f3;
     private Passenger pp1, pp2, pp3, pp4, pp5;
 
     public Tester()
@@ -16,12 +10,12 @@ public class Tester
         f1 = new Flight ("RO12", "Rome", 20);
         f2 = new Flight ("AM34", "Amsterdam", 2); // for testing purposes
         f3 = new Flight ("BE45", "Berlin", 50);  // not added to Agency
-        
+
         pp1 = new Passenger ("Amy", true);
         pp2 = new Passenger ("Ben", false);
         pp3 = new Passenger ("Chen", false);
         pp4 = new Passenger ("Dan", false);
-        pp5 = new Passenger ("Eli", false); 
+        pp5 = new Passenger ("Eli", false);
         System.out.println ("A version of Agency.");
         //Add flights to the agency
         agency.addFlight (f1); agency.addFlight (f2);
@@ -30,10 +24,10 @@ public class Tester
         System.out.println(agency.listFlights());   // you can comment this out
     }
 
-    
+
     public void doTest ()
     {
-        
+
         System.out.println("********** Responsible Client ***********");
         // write your code to check pre-conditions before making bookings
        /** Responsible client: Test precondition on bookFlight **/
@@ -46,12 +40,12 @@ public class Tester
         {
             System.out.println (f1.getCode() + " is full " );
         }
-        else        
+        else
         {
             agency.bookFlight(f1,pp1);
             System.out.println (f1.getCode ()+ " booked for " + pp1.getName());
         }
-     
+
         // try to add the second passenger to same flight
         if (!agency.isFlight(f1))
         {
@@ -61,12 +55,12 @@ public class Tester
         {
             System.out.println (f1.getCode() + " is full " );
         }
-        else        
+        else
         {
             agency.bookFlight(f1,pp2);
             System.out.println (f1.getCode() + " booked for " + pp2.getName());
         }
-      
+
         // try to add the first passenger to another flight
         if (!agency.isFlight(f2))
         {
@@ -76,12 +70,12 @@ public class Tester
         {
             System.out.println (f2.getCode() + " is full " );
         }
-        else        
+        else
         {
             agency.bookFlight(f2,pp1);
             System.out.println (f2.getCode() + " booked for " + pp1.getName());
         }
-        
+
         // add a second passenger to second flight making it full
         if (!agency.isFlight(f2))
         {
@@ -91,12 +85,12 @@ public class Tester
         {
             System.out.println (f2.getCode() + " is full " );
         }
-        else        
+        else
         {
             agency.bookFlight(f2,pp3);
             System.out.println (f2.getCode() + " booked for " + pp3.getName());
         }
-        
+
         //Testing the code
         // try to add a passenger to a non-flight
          if (!agency.isFlight(f3))
@@ -107,14 +101,14 @@ public class Tester
         {
             System.out.println (f3.getCode() + " is full " );
         }
-        else        
+        else
         {
             agency.bookFlight(f3,pp3);
             System.out.println (f3.getCode() + " booked for " + pp3.getName());
         }
-        
+
         //Testing the code
-        // try to add a passenger full flight 
+        // try to add a passenger full flight
         if (!agency.isFlight(f2))
         {
             System.out.println ("Not a flight of this agency " );
@@ -123,15 +117,15 @@ public class Tester
         {
             System.out.println (f2.getCode() + " is full " );
         }
-        else        
+        else
         {
             agency.bookFlight(f2,pp4);
             System.out.println (f2.getCode() + " booked for " + pp4.getName());
         }
-        
+
         System.out.println("\nShow all flights");
-        System.out.println(agency.listFlights());        
-       
+        System.out.println(agency.listFlights());
+
         System.out.println("********Irresponsible client (needed by EA Only !!)*************");
         // write code but DO NOT check pre-conditions before making bookings - show bad things can happen
         agency.bookFlight(f1, pp4);      // will be OK
@@ -140,11 +134,10 @@ public class Tester
         System.out.println (f3.getCode() + " booked for " + pp3.getName());
         agency.bookFlight (f2,pp4);      // flight full
         System.out.println (f3.getCode() + " booked for " + pp4.getName());
-        agency.bookFlight (f2,pp5);      // flight full 
+        agency.bookFlight (f2,pp5);      // flight full
         System.out.println (f3.getCode() + " booked for " + pp5.getName());
         System.out.println("\nShow all flights");
         System.out.println(agency.listFlights());
-
 
     }
 }
