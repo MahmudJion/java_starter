f4b7ed1c5d2e080f851e22bcaa495a2cf71bb537
MahmudJion <mahmudh.jion02@gmail.com>
2022-06-28 06:39:55 +0100

    update Tester.java

--- flights/Solutions in BlueJ/02d flightDP-string/Tester.java
+++ flights/Solutions in BlueJ/02d flightDP-string/Tester.java
@@ -1,13 +1,7 @@
-
-/**
- * Tester Class is a client of Agency
- * Explores approaches to error handling. 
- * @version 3a - Defensive Programming using return exceptions
- */
 public class Tester
 {
     private Agency agency = new Agency();
-    private Flight f1 ,f2,f3; 
+    private Flight f1 ,f2,f3;
     private Passenger pp1, pp2, pp3, pp4, pp5;
 
     public Tester()
@@ -16,13 +10,13 @@ public class Tester
         f1 = new Flight ("RO12", "Rome", 20);
         f2 = new Flight ("AM34", "Amsterdam", 2); // for testing purposes
         f3 = new Flight ("BE45", "Berlin", 50);  // not added to Agency
-        
+
         pp1 = new Passenger ("Amy", true);
         pp2 = new Passenger ("Ben", false);
         pp3 = new Passenger ("Chen", false);
         pp4 = new Passenger ("Dan", false);
-        pp5 = new Passenger ("Eli", false); 
-        
+        pp5 = new Passenger ("Eli", false);
+
         System.out.println ("A version of Agency.");
         //Add flights to the agency
         agency.addFlight (f1); agency.addFlight (f2);
@@ -30,22 +24,22 @@ public class Tester
         System.out.println("***********Flights***************");
         System.out.println(agency.listFlights());   // you can comment this out
     }
-    
+
     public void doTest()
-    {    
+    {
         System.out.println ("************Defensive Programming returning a report*************");
         System.out.println(agency.bookFlight(f1,pp1));
-        System.out.println(agency.bookFlight(f1,pp2));   
+        System.out.println(agency.bookFlight(f1,pp2));
         System.out.println(agency.bookFlight(f2,pp1));
         System.out.println(agency.bookFlight(f2,pp3));
-        System.out.println(agency.bookFlight(f3,pp1));        
-        System.out.println(agency.bookFlight(f2,pp4));        
-         
+        System.out.println(agency.bookFlight(f3,pp1));
+        System.out.println(agency.bookFlight(f2,pp4));
+
         System.out.println("\nShow all flights");
         System.out.println(agency.listFlights());
 
 
-    }       
-    
-     
+    }
+
+
 }
