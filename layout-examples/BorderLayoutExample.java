8c399fc1845587c013d80ad4f774d79fdd604b4d
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-01 06:59:51 +0100

    review and update

--- layout-examples/BorderLayoutExample.java
+++ layout-examples/BorderLayoutExample.java
@@ -6,24 +6,24 @@ public class BorderLayoutExample
     private JFrame frame = new JFrame ("Border Layout Example");
 
     private JButton northButton = new JButton ("North Button");
-    private JButton eastButton = new JButton ("East Button");    
-    private JButton southButton = new JButton ("South Button");    
-    private JButton westButton = new JButton ("West Button");     
-    
+    private JButton eastButton = new JButton ("East Button");
+    private JButton southButton = new JButton ("South Button");
+    private JButton westButton = new JButton ("West Button");
+
     private JTextField textField = new JTextField(20);
-    
-    public BorderLayoutExample() 
-    {       
-       
+
+    public BorderLayoutExample()
+    {
+
         frame.setLayout (new BorderLayout());
         frame.add (northButton, BorderLayout.NORTH);
-        frame.add (eastButton, BorderLayout.EAST);        
-        frame.add (southButton, BorderLayout.SOUTH);        
+        frame.add (eastButton, BorderLayout.EAST);
+        frame.add (southButton, BorderLayout.SOUTH);
         frame.add (westButton, BorderLayout.WEST);
-        
-        frame.add (textField, BorderLayout.CENTER);     
-        
+
+        frame.add (textField, BorderLayout.CENTER);
+
         frame.pack();
         frame.setVisible (true);
-    }        
+    }
 }
