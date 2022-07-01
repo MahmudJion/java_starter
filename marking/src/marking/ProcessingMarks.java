3063867257815ea55dcc5c07bf96a26bcaacd2c4
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-01 07:16:30 +0100

    review and update

--- marking/src/marking/ProcessingMarks.java
+++ marking/src/marking/ProcessingMarks.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package marking;
 
-/**
- *
- * @author comqaam
- */
 public class ProcessingMarks {
-  
+
     public int processMark1(int mark)
     {
        if (mark<= 40) {
@@ -20,7 +11,7 @@ public class ProcessingMarks {
            return 1;
        }
     }
-    
+
     public boolean processMark2(int mark)
     {
        if (mark<= 40) {
@@ -30,7 +21,7 @@ public class ProcessingMarks {
            return true;
        }
     }
-    
+
     public String processMark3(int mark)
     {
        if (mark<= 40) {
@@ -41,4 +32,4 @@ public class ProcessingMarks {
        }
     }
 }
-   
+
