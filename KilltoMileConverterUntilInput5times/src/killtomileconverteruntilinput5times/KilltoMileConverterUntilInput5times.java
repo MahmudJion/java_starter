c2a45da765dcc3f3763b611754b681000cc16365
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-01 06:59:38 +0100

    review and update

--- KilltoMileConverterUntilInput5times/src/killtomileconverteruntilinput5times/KilltoMileConverterUntilInput5times.java
+++ KilltoMileConverterUntilInput5times/src/killtomileconverteruntilinput5times/KilltoMileConverterUntilInput5times.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package KilltoMileConverterUntilInput5times.src.killtomileconverteruntilinput5times;
 
-/**
- *
- * @author lenovo
- */
 import java.util.Scanner;
 
 public class KilltoMileConverterUntilInput5times {
