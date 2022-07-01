b10b27c0542f32af3463480416448794f06a7e3d
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-01 07:03:06 +0100

    review and update

--- layout-examples/LayoutTester.java
+++ layout-examples/LayoutTester.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of class LayoutTester here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class LayoutTester
 {
     public static void main(String args[]){
