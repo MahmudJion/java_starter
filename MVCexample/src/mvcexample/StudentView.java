825bb83135513b6bba75374069ec332718198d57
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-11 07:24:00 +0100

    review and update

--- MVCexample/src/mvcexample/StudentView.java
+++ MVCexample/src/mvcexample/StudentView.java
@@ -1,15 +1,5 @@
 package mvcexample;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author gd17aaq
- */
 public class StudentView {
    public void printStudentDetails(String studentName, String studentRollNo){
       System.out.println("Student: ");
