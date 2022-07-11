db02266f632926cc0dfdc904c2fb96cf0354651c
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-11 07:24:48 +0100

    review and update

--- MVCexample/src/mvcexample/StudentController.java
+++ MVCexample/src/mvcexample/StudentController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mvcexample;
 
 /**
@@ -19,43 +14,31 @@ public class StudentController {
       model.setRollNo("10");
       return model;
    }
-   
-   
-   
-   
-   
+
    public StudentController(){
       this.retriveStudentFromDatabase();
       this.view = new StudentView();
-      
+
    }
 
-   
-   
-   
    public void setStudentName(String name){
-      model.setName(name);		
+      model.setName(name);
    }
 
    public String getStudentName(){
-      return model.getName();		
+      return model.getName();
    }
 
    public void setStudentRollNo(String rollNo){
-      model.setRollNo(rollNo);		
+      model.setRollNo(rollNo);
    }
 
    public String getStudentRollNo(){
-      return model.getRollNo();		
+      return model.getRollNo();
    }
 
-   public void updateView(){				
+   public void updateView(){
       view.printStudentDetails(model.getName(), model.getRollNo());
-   }	
-
-
-
-
-
+   }
 
 }
