28c81319b7a968099484c41d04ea6279f31ed629
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-11 07:24:42 +0100

    review and update

--- MVCexample/src/mvcexample/Student.java
+++ MVCexample/src/mvcexample/Student.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mvcexample;
 
 /**
@@ -12,19 +7,19 @@ package mvcexample;
 public class Student {
    private String rollNo;
    private String name;
-   
+
    public String getRollNo() {
       return rollNo;
    }
-   
+
    public void setRollNo(String rollNo) {
       this.rollNo = rollNo;
    }
-   
+
    public String getName() {
       return name;
    }
-   
+
    public void setName(String name) {
       this.name = name;
    }
