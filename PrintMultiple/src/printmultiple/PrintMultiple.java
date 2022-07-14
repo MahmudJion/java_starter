b54ecd6e42cca2e1240e9f52ca286a6917b68812
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-14 12:25:27 +0100

    review and update

--- PrintMultiple/src/printmultiple/PrintMultiple.java
+++ PrintMultiple/src/printmultiple/PrintMultiple.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package printmultiple;
-
 import java.util.Scanner;
 
-/**
- *
- * @author lenovo
- */
 public class PrintMultiple {
 
     /**
      * @param args the command line arguments
      */
-    
+
     static Scanner console = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -25,7 +15,7 @@ public class PrintMultiple {
         printMultiple("World", -1);
         printMultiple("Love", 1);
     }
-    
+
     static void printMultiple(String str, int num){
        while(num > 0){
          System.out.print(str+" ");
