c94701621687a6963f2e3652d661c4d1202eed5c
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-14 12:20:52 +0100

    review and update

--- NumberAddition/NumberAdditionUI.java
+++ NumberAddition/NumberAdditionUI.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package NumberAddition;
 
-/**
- *
- * @author mh21act
- */
 public class NumberAdditionUI extends javax.swing.JFrame {
 
     /**
