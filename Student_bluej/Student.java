bf2fc4fe6fdea59c4ec32bf122da10838b7128cd
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-15 19:53:32 +0100

    review and update

--- Student_bluej/Student.java
+++ Student_bluej/Student.java
@@ -1,12 +1,5 @@
 package Student_bluej;
 
-
-/**
- * Write a description of class Student here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Student
 {
     private String name;
