4d6b8653a9a43b1e978674827f4782efb8bd886b
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-19 07:11:38 +0100

    review and update

--- test/marking/ProcessMarks1Test.java
+++ test/marking/ProcessMarks1Test.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package marking;
 
 import org.junit.After;
@@ -12,10 +7,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author comqaam
- */
 public class ProcessMarks1Test {
     ProcessingMarks myUnit;
 
