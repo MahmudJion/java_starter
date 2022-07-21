b9e86fa8a58755ee4c849381c4742d3c43bef368
MahmudJion <mahmudh.jion02@gmail.com>
2022-07-21 06:35:27 +0100

    review and update

--- zuul-with-enums-v1/src/zuul/Parser.java
+++ zuul-with-enums-v1/src/zuul/Parser.java
@@ -1,24 +1,7 @@
 package zuul;
 import java.util.Scanner;
 import java.util.StringTokenizer;
-
-/**
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
- * 
- * This parser reads user input and tries to interpret it as an "Adventure"
- * command. Every time it is called it reads a line from the terminal and
- * tries to interpret the line as a two-word command. It returns the command
- * as an object of class Command.
- *
- * The parser has a set of known command words. It checks user input against
- * the known commands, and if the input is not one of the known commands, it
- * returns a command object that is marked as an unknown command.
- * 
- * @author  Michael Kolling and David J. Barnes
- * @version 2006.03.30
- */
-public class Parser 
+public class Parser
 {
     private CommandWords commands;  // holds all valid command words
     private Scanner reader;         // source of command input
@@ -26,7 +9,7 @@ public class Parser
     /**
      * Create a parser to read from the terminal window.
      */
-    public Parser() 
+    public Parser()
     {
         commands = new CommandWords();
         reader = new Scanner(System.in);
@@ -35,7 +18,7 @@ public class Parser
     /**
      * @return The next command from the user.
      */
-    public Command getCommand() 
+    public Command getCommand()
     {
         String inputLine;   // will hold the full input line
         String word1 = null;
