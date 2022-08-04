1f14fc0a033cbb8c78f311b8c1d5fccf581e0d98
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2022-08-04 07:31:20 +0100

    review and update

--- Sales.java
+++ Sales.java
@@ -9,7 +9,7 @@ public class Sales {
         int eggsWanted;
         StockItem product2 = new StockItem("Eggs (box of 6)", 175, 10);
         System.out.println("How many egges do you want?");
-        //eggsWanted = reader.nextInt();
+        eggsWanted = reader.nextInt();
         product2.doSale(reader.nextInt());
         product2.printReport();
     }
